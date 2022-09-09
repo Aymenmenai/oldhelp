92d498a0a4d1b003df8938214b4243340e94cb97
Aymen Menai <aymenmenai8@gmail.com>
2022-09-09 15:52:37 +0100

    add-style commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  // content: [
+  //   "./pages/**/*.{js,ts,jsx,tsx}",
+  //   "./components/**/*.{js,ts,jsx,tsx}",
+  // ],
+  // content: [], <-- not working
+  // content: ['./pages/**/*.{js,ts,jsx,tsx}'], <-- not working
+  content: ['production/oright/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
