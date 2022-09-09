4360b3515f900b07f049e265a05db3b92c723878
Aymen Menai <aymenmenai8@gmail.com>
2022-09-09 16:16:55 +0100

    add-style-2 commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{html,js}',
-    './components/**/*.{html,js}',
-    './public/index.html',
-    './src/**/*.{html,js}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
