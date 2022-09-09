754668e428782853fa66ee8604701a1e96cf47c7
Aymenmenai <Aymenmenai8@gmail.com>
2022-09-09 14:22:09 +0000

    Deploying to gh-pages from @ Aymenmenai/Oright@3e940ea6f918d641de75bc6f7c03caf576695daa 🚀

---
+++ _next/static/-ZO3rSfo79NRGtseBthtU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-c0a772c11002e1aa.js","static/chunks/pages/index-fc448d9518381de2.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
